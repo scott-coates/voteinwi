2c1c03c60b63b46e413f819f98a70ed6b437f9a2
Scott Coates <scoarescoare@gmail.com>
2020-06-29 18:25:22 -0500

    added Info Box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,61 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
+
 import { Collapse } from "react-collapse";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 // import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+
+function InfoBox(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div {...props} className="shadow">
+      <article className="border-b" onClick={() => setIsOpen(!isOpen)}>
+        <div className="border-transparent">
+          <header className="flex justify-between items-center cursor-pointer select-none">
+            <div className="bg-indigo-900  py-4 lg:px-8 w-full">
+              <div
+                className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+                role="alert"
+              >
+                <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+                  <svg
+                    className="fill-current opacity-75 h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm2-13c0 .28-.21.8-.42 1L10 9.58c-.57.58-1 1.6-1 2.42v1h2v-1c0-.29.21-.8.42-1L13 9.42c.57-.58 1-1.6 1-2.42a4 4 0 1 0-8 0h2a2 2 0 1 1 4 0zm-3 8v2h2v-2H9z" />
+                  </svg>
+                </span>
+                <span className="font-semibold mr-2 text-left flex-auto">
+                  {props.title}
+                </span>
+                <svg
+                  className="fill-current opacity-75 h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+                </svg>
+              </div>
+            </div>
+          </header>
+          <Collapse isOpened={isOpen}>
+            <div className="pl-8 pr-8 pb-5 text-grey-darkest">
+              {props.children}
+            </div>
+          </Collapse>
+        </div>
+      </article>
+    </div>
+  );
+}
+InfoBox.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 function IndexPage() {
   // const now = new Date();
@@ -13,9 +65,9 @@ function IndexPage() {
   // debugger;
 
   // Declare a new state variable, which we'll call "count"
-  const [info1, setDisplayInfo1] = useState(false);
-  const [info2, setDisplayInfo2] = useState(false);
-  const [info3, setDisplayInfo3] = useState(false);
+  // const [info1, setDisplayInfo1] = useState(false);
+  // const [info2, setDisplayInfo2] = useState(false);
+  // const [info3, setDisplayInfo3] = useState(false);
 
   return (
     <Layout>
@@ -110,8 +162,21 @@ function IndexPage() {
           <div className="leading-loose p-3">
             <p className="mb-3">
               First, you can register to vote in person at your polling place on
-              Election Day. Make sure you bring both a proof of residency
-              document and a form of I.D. along to register on Election Day.
+              Election Day. Make sure you bring both a
+              <a
+                className="no-underline text-blue-700 hover:text-blue-800"
+                href="https://myvote.wi.gov/en-us/ProofofResidence"
+              >
+                &nbsp;proof of residency document
+              </a>
+              &nbsp;and a
+              <a
+                className="no-underline text-blue-700 hover:text-blue-800"
+                href="https://myvote.wi.gov/en-us/PhotoIDRequired"
+              >
+                &nbsp;form of I.D
+              </a>
+              &nbsp;along to register on Election Day.
             </p>
 
             <p className="mb-3">
@@ -147,7 +212,10 @@ function IndexPage() {
                   href="https://elections.wi.gov/sites/elections.wi.gov/files/2019-11/EL-131%20Redesign%20fillable_1.pdf"
                 >
                   &nbsp;Click here to print the form.
-                </a>
+                </a>{" "}
+                <span className="font-bold">
+                  &nbsp;Bring your photo ID and proof of residency.
+                </span>
               </li>
             </ul>
             <p className="mb-3">
@@ -155,66 +223,28 @@ function IndexPage() {
               and a form of I.D. along no matter which kind of registration you
               choose.
             </p>
-            <div className="shadow">
-              <article
-                className="border-b"
-                onClick={() => setDisplayInfo1(!info1)}
-              >
-                <div className="border-transparent">
-                  <header className="flex justify-between items-center cursor-pointer select-none">
-                    <div className="bg-indigo-900  py-4 lg:px-8 w-full">
-                      <div
-                        className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-                        role="alert"
-                      >
-                        <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-                          <svg
-                            className="fill-current opacity-75 h-4 w-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                          >
-                            <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm2-13c0 .28-.21.8-.42 1L10 9.58c-.57.58-1 1.6-1 2.42v1h2v-1c0-.29.21-.8.42-1L13 9.42c.57-.58 1-1.6 1-2.42a4 4 0 1 0-8 0h2a2 2 0 1 1 4 0zm-3 8v2h2v-2H9z" />
-                          </svg>
-                        </span>
-                        <span className="font-semibold mr-2 text-left flex-auto">
-                          Need the mailing address to your local election
-                          office?
-                        </span>
-                        <svg
-                          className="fill-current opacity-75 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
-                        </svg>
-                      </div>
-                    </div>
-                  </header>
-                  <Collapse isOpened={info1}>
-                    <div>
-                      <div className="pl-8 pr-8 pb-5 text-grey-darkest">
-                        <ul className="pt-4">
-                          <li className="pb-2">
-                            Municipal clerks are in charge of elections for your
-                            city, town, or village. Your municipal clerk handles
-                            voter registrations and absentee ballots for voters
-                            in their municipality.
-                          </li>
-                          <li className="pb-2">
-                            <a
-                              className="no-underline text-blue-700 hover:text-blue-800"
-                              href="https://myvote.wi.gov/en-US/MyMunicipalClerk"
-                            >
-                              Click here to locate your municipal clerk.
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </Collapse>
-                </div>
-              </article>
-            </div>
+            <InfoBox
+              className="uppercase"
+              title="Need the mailing address to your local election
+                          office?"
+            >
+              <ul className="pt-4">
+                <li className="pb-2">
+                  Municipal clerks are in charge of elections for your city,
+                  town, or village. Your municipal clerk handles voter
+                  registrations and absentee ballots for voters in their
+                  municipality.
+                </li>
+                <li className="pb-2">
+                  <a
+                    className="no-underline text-blue-700 hover:text-blue-800"
+                    href="https://myvote.wi.gov/en-US/MyMunicipalClerk"
+                  >
+                    Click here to locate your municipal clerk.
+                  </a>
+                </li>
+              </ul>
+            </InfoBox>
           </div>
         </section>
 
@@ -327,65 +357,24 @@ function IndexPage() {
               </a>
               .
             </p>
-            <div className="shadow">
-              <article
-                className="border-b"
-                onClick={() => setDisplayInfo2(!info1)}
-              >
-                <div className="border-transparent">
-                  <header className="flex justify-between items-center cursor-pointer select-none">
-                    <div className="bg-indigo-900  py-4 lg:px-8 w-full">
-                      <div
-                        className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-                        role="alert"
-                      >
-                        <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-                          <svg
-                            className="fill-current opacity-75 h-4 w-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                          >
-                            <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm2-13c0 .28-.21.8-.42 1L10 9.58c-.57.58-1 1.6-1 2.42v1h2v-1c0-.29.21-.8.42-1L13 9.42c.57-.58 1-1.6 1-2.42a4 4 0 1 0-8 0h2a2 2 0 1 1 4 0zm-3 8v2h2v-2H9z" />
-                          </svg>
-                        </span>
-                        <span className="font-semibold mr-2 text-left flex-auto">
-                          Need to find your local election office?
-                        </span>
-                        <svg
-                          className="fill-current opacity-75 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
-                        </svg>
-                      </div>
-                    </div>
-                  </header>
-                  <Collapse isOpened={info2}>
-                    <div>
-                      <div className="pl-8 pr-8 pb-5 text-grey-darkest">
-                        <ul className="pt-4">
-                          <li className="pb-2">
-                            Municipal clerks are in charge of elections for your
-                            city, town, or village. Your municipal clerk handles
-                            voter registrations and absentee ballots for voters
-                            in their municipality.
-                          </li>
-                          <li className="pb-2">
-                            <a
-                              className="no-underline text-blue-700 hover:text-blue-800"
-                              href="https://myvote.wi.gov/en-US/MyMunicipalClerk"
-                            >
-                              Click here to locate your municipal clerk.
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </Collapse>
-                </div>
-              </article>
-            </div>{" "}
+            <InfoBox title="Need to find your local election office?">
+              <ul className="pt-4">
+                <li className="pb-2">
+                  Municipal clerks are in charge of elections for your city,
+                  town, or village. Your municipal clerk handles voter
+                  registrations and absentee ballots for voters in their
+                  municipality.
+                </li>
+                <li className="pb-2">
+                  <a
+                    className="no-underline text-blue-700 hover:text-blue-800"
+                    href="https://myvote.wi.gov/en-US/MyMunicipalClerk"
+                  >
+                    Click here to locate your municipal clerk.
+                  </a>
+                </li>
+              </ul>
+            </InfoBox>{" "}
           </div>
         </section>
 
@@ -464,70 +453,33 @@ function IndexPage() {
               </a>{" "}
             </p>
           </div>
-          <div className="shadow mb-3">
-            <article
-              className="border-b"
-              onClick={() => setDisplayInfo3(!info3)}
+          <div className="mb-3">
+            <InfoBox
+              title="Want to volunteer for your local election office?
+"
             >
-              <div className="border-transparent">
-                <header className="flex justify-between items-center cursor-pointer select-none">
-                  <div className="bg-indigo-900  py-4 lg:px-8 w-full">
-                    <div
-                      className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-                      role="alert"
-                    >
-                      <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-                        <svg
-                          className="fill-current opacity-75 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm2-13c0 .28-.21.8-.42 1L10 9.58c-.57.58-1 1.6-1 2.42v1h2v-1c0-.29.21-.8.42-1L13 9.42c.57-.58 1-1.6 1-2.42a4 4 0 1 0-8 0h2a2 2 0 1 1 4 0zm-3 8v2h2v-2H9z" />
-                        </svg>
-                      </span>
-                      <span className="font-semibold mr-2 text-left flex-auto">
-                        Want to volunteer for your local election office?
-                      </span>
-                      <svg
-                        className="fill-current opacity-75 h-4 w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
-                      </svg>
-                    </div>
-                  </div>
-                </header>
-                <Collapse isOpened={info3}>
-                  <div>
-                    <div className="pl-8 pr-8 pb-5 text-grey-darkest">
-                      <ul className="pt-4">
-                        <li className="pb-2">
-                          Municipal clerks are in charge of elections for your
-                          city, town, or village. Your municipal clerk handles
-                          voter registrations and absentee ballots for voters in
-                          their municipality.
-                        </li>
-                        <li className="pb-2">
-                          Each city, village and town in Wisconsin is
-                          responsible for setting the dates and hours of
-                          in-person absentee voting for their municipality. To
-                          find the dates and hours for in-person absentee voting
-                          where you live,
-                          <a
-                            className="no-underline text-blue-700 hover:text-blue-800"
-                            href="https://myvote.wi.gov/en-US/MyMunicipalClerk"
-                          >
-                            {" "}
-                            click here.
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </Collapse>
-              </div>
-            </article>
+              <ul className="pt-4">
+                <li className="pb-2">
+                  Municipal clerks are in charge of elections for your city,
+                  town, or village. Your municipal clerk handles voter
+                  registrations and absentee ballots for voters in their
+                  municipality.
+                </li>
+                <li className="pb-2">
+                  Each city, village and town in Wisconsin is responsible for
+                  setting the dates and hours of in-person absentee voting for
+                  their municipality. To find the dates and hours for in-person
+                  absentee voting where you live,
+                  <a
+                    className="no-underline text-blue-700 hover:text-blue-800"
+                    href="https://myvote.wi.gov/en-US/MyMunicipalClerk"
+                  >
+                    {" "}
+                    click here.
+                  </a>
+                </li>
+              </ul>
+            </InfoBox>{" "}
           </div>{" "}
           <div className="leading-loose p-3">
             <p className="mb-3 font-bold">
